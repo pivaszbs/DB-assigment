6ded26a35c31464a830590de4e8f70083fccc36d
Gewery <35733988+Gewery@users.noreply.github.com>
2018-11-26 17:14:18 +0300

    Added query 6

--- BDschema.py
+++ BDschema.py
@@ -1,8 +1,4 @@
 from peewee import *
-from numpy.random import random_integers as ri
-from numpy.random import random
-from random import randrange
-import datetime
 from models.Car import Car
 from models.Provider import Provider
 from models.Customer import Customer
@@ -33,7 +29,33 @@ def execute_queries():
         print(*cursor.execute(query))
 
 
+'''QUERY 6'''
+def query_top_3_popular_locations_for_evety_time_slot():
+    cursor = db.cursor()
+    time_slots = [(7, 10), (12, 14), (17, 19)]
+    text_time_slots = ['Morning(9AM-10AM)', 'Afternoon(12AM-2PM)', 'Evening(5PM-7PM)']
+
+    for time_slot in range(3):
+        print('--------' + text_time_slots[time_slot] + '--------')
+        print('Top-3 pickup locations:')
+        time_from = time_slots[time_slot][0]
+        time_to = time_slots[time_slot][1]
+        locations = cursor.execute(str('SELECT pickup_location, count(pickup_location) FROM tripevent WHERE CAST(strftime(\'%H\', pickup_time) AS INT) >= ' + str(time_from) + ' AND CAST(strftime(\'%H\', pickup_time) AS INT) < ' + str(time_to) + ' GROUP BY pickup_location ORDER BY count(pickup_location) DESC LIMIT 3'))
+        for loc in locations:
+            print(loc[0] + ' (' + str(loc[1]) + ' times)')
+
+        print('\nTop-3 destination locations:')
+        locations = cursor.execute(str('SELECT destination_location, count(destination_location) FROM tripevent WHERE CAST(strftime(\'%H\', pickup_time) AS INT) >= ' + str(time_from) + ' AND CAST(strftime(\'%H\', pickup_time) AS INT) < ' + str(time_to) + ' GROUP BY destination_location ORDER BY count(destination_location) DESC LIMIT 3'))
+        for loc in locations:
+            print(loc[0] + ' (' + str(loc[1]) + ' times)')
+
+        print('\n')
+
+
+
+
 create_tables()
+
 DataGenerator.generate_car()
 DataGenerator.generate_workshop()
 DataGenerator.generate_socket()
@@ -44,4 +66,6 @@ DataGenerator.generate_charging_station()
 DataGenerator.generate_trip_event()
 DataGenerator.generate_repairing_event()
 DataGenerator.generate_charging_event()
-#execute_queries()
+# execute_queries()
+
+query_top_3_popular_locations_for_evety_time_slot()
